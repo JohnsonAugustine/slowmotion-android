7e1d5552da9246f858002570100ac737319484f9
Boyuan Yang <073plan@gmail.com>
2015-09-04 22:38:48 +0800

    various unimportant changes

---
+++ app/src/main/java/me/hosiet/slowmotion/DebugActivity.java
@@ -0,0 +1,18 @@
+package me.hosiet.slowmotion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * DebugActivity
+ * Created by hosiet on 15-8-30.
+ */
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_debug);
+    }
+}
