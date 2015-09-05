5a1c12cd4e3d051e2e59fbd570990446a34d660c
Boyuan Yang <073plan@gmail.com>
2015-09-05 10:31:36 +0800

    import drawer library; not compilable yet

---
+++ app/src/main/java/me/hosiet/slowmotion/MusicActivity.java
@@ -0,0 +1,19 @@
+package me.hosiet.slowmotion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Music Activity.
+ *
+ * Created by hosiet on 15-9-5.
+ */
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+
+    }
+}
